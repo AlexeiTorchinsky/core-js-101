15db4b1b117da4057399108d570554a4a16a36fd
Alexei Torchinsky <alextor1005@gmail.com>
2022-11-29 00:45:56 +0300

    done some tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -35,12 +35,9 @@ function findElement(arr, value) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  // const arr = new Array(len);
-  // arr.fill(1);
-  // const initialValue = 1;
-  // return arr.reduce((el) => el + 2, initialValue);
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return [...Array(len)].map((_, i) => 2 * i + 1);
+  // throw new Error('Not implemented');
 }
 
 
@@ -104,14 +101,10 @@ function getArrayOfStrings(arr) {
  *    [ 1, 2, 3, 4, 5, 'false' ]         => [ 1, 2, 3, 4, 5, 'false' ]
  *    [ false, 0, NaN, '', undefined ]   => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
-  // return arr.map(functoin (el) {
-  //   if (el === true) {
-  //     return el;
-  //   }
-  // });
+function removeFalsyValues(arr) {
+  return arr.filter((el) => el);
 }
+
 
 /**
  * Returns the array of uppercase strings from the specified array
@@ -156,8 +149,8 @@ function getStringsLength(arr) {
  *    [ 1, 3, 4, 5 ], 2, 1  => [ 1, 2, 3, 4, 5 ]
  *    [ 1, 'b', 'c'], 0, 'x'  => [ 'x', 1, 'b', 'c' ]
  */
-function insertItem(/* arr, item, index */) {
-  throw new Error('Not implemented');
+function insertItem(arr, item, index) {
+  return arr.splice(index, 0, item);
 }
 
 /**
@@ -170,8 +163,8 @@ function insertItem(/* arr, item, index */) {
  *    [ 1, 3, 4, 5 ], 2 => [ 1, 3 ]
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'a', 'b', 'c' ]
  */
-function getHead(/* arr, n */) {
-  throw new Error('Not implemented');
+function getHead(arr, n) {
+  return arr.slice(0, n);
 }
 
 
@@ -185,8 +178,8 @@ function getHead(/* arr, n */) {
  *    [ 1, 3, 4, 5 ], 2  => [ 4, 5 ]
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  return arr.slice(-n);
 }
 
 
@@ -210,8 +203,8 @@ function getTail(/* arr, n */) {
  *    +'20,21,22,23,24\n'
  *    +'30,31,32,33,34'
  */
-function toCsvText(/* arr */) {
-  throw new Error('Not implemented');
+function toCsvText(arr) {
+  return arr.map((el) => el.toString()).join('\n');
 }
 
 /**
