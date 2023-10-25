57582c3628003c0921913612fdbdc7cb9578dd0a
Alexei Torchinsky <alextor1005@gmail.com>
2023-10-25 09:34:53 +0300

    feat: done one more array task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -299,12 +299,9 @@ function propagateItemsByPositionIndex(arr) {
  *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
-function get3TopItems(/* arr */) {
-  throw new Error('Not implemented');
-  // let arr1 = [];
-  // if (arr.length === 0) {
-  //   return arr;
-  // }
+function get3TopItems(arr) {
+  // throw new Error('Not implemented');
+  return arr.sort((a, b) => b - a).slice(0, 3);
 }
 
 
